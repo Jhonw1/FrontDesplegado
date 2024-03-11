3c068f283b73c61629d688454535b173528752c2
Juan Jose <juanjo2003gg@gmail.com>
2024-03-11 02:15:16 -0500

    Conexion chat

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -16,7 +16,7 @@ function Chat() {
         e.preventDefault(); 
 
         try {
-            const response = await fetch('http://localhost:10101/chat', {
+            const response = await fetch('https://chatdesplegado.onrender.com', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
